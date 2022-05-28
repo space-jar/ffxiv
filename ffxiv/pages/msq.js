c8f12cd23192b4d5d4d0b10e1a9b46f4f2def00f
Jared Roder <jroder@liveareacx.com>
2022-05-27 23:35:45 -0500

    Add new SSR (server side rendered) page with xivapi request

---
+++ ffxiv/pages/msq.js
@@ -0,0 +1,15 @@
+function Msq({ data }) {
+    return <h1>{data.Character.Name}</h1>;
+}
+
+// This gets called on every request
+export async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await fetch(`https://xivapi.com/character/8728612`)
+    const data = await res.json()
+
+    // Pass data to the page via props
+    return { props: { data } }
+}
+
+export default Msq;
