0e8629952c6073b1c7af81ad0d1477e3b47122e2
Jared Roder <jroder@liveareacx.com>
2022-05-30 02:06:01 -0500

    Early attempt to get Quest data using xxiv api's ElasticSearch capabilities.

--- ffxiv/pages/msq.js
+++ ffxiv/pages/msq.js
@@ -1,12 +1,32 @@
 function Msq({ data }) {
-    return <h1>{data.Character.Name}</h1>;
+    //return <div><h1>{data.Character.Name}</h1><div>{JSON.stringify(data.Character)}</div></div>;
+    return <p>{JSON.stringify(data)}</p>
+}
+
+async function xxivRequest() {
+    const res = await fetch (process.env.FFXIV_API_URL + '/search?page=2', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "indexes": "quest",
+            "columns": "ID,Name",
+            "body": {
+            }
+        })
+    })
+
+    const result = await res.json()
+
+    return result;
 }
 
 // This gets called on every request
 export async function getServerSideProps() {
     // Fetch data from external API
-    const res = await fetch(process.env.FFXIV_API_URL + '/character/8728612')
-    const data = await res.json()
+    const data = await xxivRequest()
 
     // Pass data to the page via props
     return { props: { data } }
