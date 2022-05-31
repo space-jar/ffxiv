aa03cf12263ad200b7021bcbae6bb23198798a28
Jared Roder <jroder@liveareacx.com>
2022-05-30 22:08:29 -0500

    Installed xivapi/js to help me not have to write so many data queries.

--- ffxiv/pages/msq.js
+++ ffxiv/pages/msq.js
@@ -1,26 +1,28 @@
 function Msq({ data }) {
     //return <div><h1>{data.Character.Name}</h1><div>{JSON.stringify(data.Character)}</div></div>;
-    return <p>{JSON.stringify(data)}</p>
+    return <b>{JSON.stringify(data)}</b>
 }
 
 async function xxivRequest() {
-    const res = await fetch (process.env.FFXIV_API_URL + '/search?page=2', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
+    const XIVAPI = require('@xivapi/js')
+    const xiv = new XIVAPI()
+    let res
+
+    try {
+        res = await xiv.search({
             "indexes": "quest",
             "columns": "ID,Name",
-            "body": {
-            }
+            "page": 2,
+            "body": {}
+        }, {
+            private_key: process.env.FFXIV_API_KEY_V
         })
-    })
+    } catch (e) {
+        // TODO: How should xivapi errors be logged? Customer logger module? Another package?
+        console.error(`${process.env.FFXIV_API_URL} request error`, e)
+    }
 
-    const result = await res.json()
-
-    return result;
+    return res;
 }
 
 // This gets called on every request
